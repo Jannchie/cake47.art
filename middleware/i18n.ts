353806eab0c1945aa5b14964cc1cc28d0a9f5724
Jannchie <jannchie@gmail.com>
2023-08-29 02:37:20 +0900

    :adhesive_bandage: fix: i18n detect

--- middleware/i18n.ts
+++ middleware/i18n.ts
@@ -7,6 +7,12 @@ export default defineNuxtRouteMiddleware((to) => {
   // get preferred language from browser
   if (typeof window === 'undefined')
     return
-  const preferredLanguage = (typeof navigator === 'undefined') ? 'en' : navigator.language.split('-')[0]
+  let preferredLanguage = (typeof navigator !== 'undefined') ? navigator.language : 'en'
+  if (preferredLanguage === 'zh-TW' || preferredLanguage === 'zh-HK' || preferredLanguage === 'zh')
+    preferredLanguage = 'zh-CN'
+  else if (preferredLanguage === 'ja-JP' || preferredLanguage === 'ja')
+    preferredLanguage = 'ja'
+  if (!locales.includes(preferredLanguage))
+    preferredLanguage = 'en'
   return navigateTo(`/${preferredLanguage}${to.path}`)
 })
