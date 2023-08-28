05144494a178cfa7ab863e26c5b854d039813d30
Jannchie <jannchie@gmail.com>
2023-08-29 01:10:36 +0900

    :art: chore: social link width

--- components/SocialLink.vue
+++ components/SocialLink.vue
@@ -16,7 +16,7 @@ if (!target)
   <NuxtLink
     :href="href"
     :target="target"
-    class="min-w-32 flex gap-4 items-center hover:bg-neutral-7/10 transition-all px-2 py-1 rounded cursor-pointer text-neutral-7"
+    class="min-w-48 flex gap-4 items-center hover:bg-neutral-7/10 transition-all px-2 py-1 rounded cursor-pointer text-neutral-7"
   >
     <i class="text-2xl" :class="icon" />
     <div>
