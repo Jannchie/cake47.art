4ed8b3e452014a66f1d3676287921a4a6c4d0a42
Jannchie <jannchie@gmail.com>
2023-08-29 03:08:11 +0900

    :adhesive_bandage: fix: better locale detect

--- middleware/i18n.ts
+++ middleware/i18n.ts
@@ -4,15 +4,23 @@ export default defineNuxtRouteMiddleware((to) => {
   const { locale } = to.params as { locale: string }
   if (locales.includes(locale))
     return
+
   // get preferred language from browser
-  if (typeof window === 'undefined')
-    return
-  let preferredLanguage = (typeof navigator !== 'undefined') ? navigator.language : 'en'
-  if (preferredLanguage === 'zh-TW' || preferredLanguage === 'zh-HK' || preferredLanguage === 'zh')
-    preferredLanguage = 'zh-CN'
-  else if (preferredLanguage === 'ja-JP' || preferredLanguage === 'ja')
-    preferredLanguage = 'ja'
-  if (!locales.includes(preferredLanguage))
-    preferredLanguage = 'en'
-  return navigateTo(`/${preferredLanguage}${to.path}`)
+  const headers = useRequestHeaders()
+  try {
+    const preferredLanguages = headers['accept-language'].split(',').map(d => d.split(';')[0])
+    for (const preferredLanguage of preferredLanguages) {
+      let trueLanguage = preferredLanguage
+      if (trueLanguage === 'zh-TW' || trueLanguage === 'zh-HK' || trueLanguage === 'zh')
+        trueLanguage = 'zh-CN'
+      else if (trueLanguage === 'ja-JP' || trueLanguage === 'ja')
+        trueLanguage = 'ja'
+      if (trueLanguage !== 'en' && locales.includes(trueLanguage))
+        return navigateTo(`/${trueLanguage}${to.path}`)
+    }
+  }
+  catch (e) {
+    console.error(e)
+  }
+  return navigateTo(`/en${to.path}`)
 })
